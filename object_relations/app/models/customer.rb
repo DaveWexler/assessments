b09d2100aa8d98589b9d314626f7b3b0bf235335
DaveWexler <dwexler@gmail.com>
2016-09-08 12:56:41 -0400

    finish Ruby part

--- object_relations/app/models/customer.rb
+++ object_relations/app/models/customer.rb
@@ -1,8 +1,26 @@
 class Customer
-  
+
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def add_review(text, restaurant)
+    review = Review.new(text, restaurant, self)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|customer| customer.name == name}
+  end
 end
 
 # customer = customer.add_review('it was good', Restaurant.new)
 # a new review tied ot the restaurant
 # and tied to the customer
-
--- object_relations/app/models/review.rb
+++ object_relations/app/models/review.rb
@@ -1,4 +1,18 @@
 class Review
-  
-end
 
+  attr_reader :text, :restaurant, :customer
+
+  @@all = []
+
+  def initialize(text, restaurant, customer)
+    @text = text
+    @restaurant = restaurant
+    @customer = customer
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
