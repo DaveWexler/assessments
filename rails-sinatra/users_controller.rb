3ba63148f4c8c77f993221c5a69ccf7f89d3f64d
DaveWexler <dwexler@gmail.com>
2016-09-08 14:21:06 -0400

    done with Rails/Sinatra part

--- rails-sinatra/users_controller.rb
+++ rails-sinatra/users_controller.rb
@@ -6,17 +6,45 @@ class UsersController < ApplicationController
   # /users/new
 
   # create
+  def create
+    @user = User.create(params[user])
+    redirect_to user_path(@user)
+  end
+  # /users with a post request (e.g. on form submission to create new user)
 
   # index
+  def index
+    @users = User.all
+  end
+  # /users (GET request)
 
   # show
+  def show
+    @user = User.find_by(params[:id])
+  end
+  # /users/:id
 
   # edit
+  def edit
+    @user = User.find_by(params[:id])
+  end
+  # /users/:id/edit
 
   # update
+  def update
+    @user = User.find_by(params[:id])
+    @user.update(params[user])
+    redirect_to user_path(@user)
+  end
+  # /users/:id (with a PATCH request)
 
   # destroy
-
+  def destroy
+    @user = User.find_by(params[:id])
+    @user.destroy
+    redirect_to action: "index"
+  end
+  # /users/:id (with DELETE request)
 end
 
 
